89d33f1339485472966c999ab4811f153a2e53bb
Ilya <karakulinilia@gmail.com>
2025-09-18 15:01:30 +0700

    bug fix

--- GenAI-1-08/1.py
+++ GenAI-1-08/1.py
@@ -1,31 +1,42 @@
-# pip install transformers torch
-import torch
 from transformers import pipeline
+from transformers import logging
+
+logging.set_verbosity_error()
+
 
 def show_topk(results, k=3):
     for i, r in enumerate(results[:k], 1):
         print(f"{i}. {r['token_str']}  (score={r['score']:.4f})")
 
+def BERT(model_version, device='cpu'):
+    model = pipeline("fill-mask", model=model_version, device=device)
+    res = model(text, top_k=3)
+    return res
+
+def test(predict):
+    gold_variants = {"России", "Российской Федерации", "РФ"}
+    pred_tokens = {r["token_str"].strip() for r in predict[:3]}
+    hit = len(gold_variants & pred_tokens) > 0.
+    return hit
+
 text = "Москва - столица [MASK]."
 
-device = 0 if torch.cuda.is_available() else -1
-
 print("En BERT")
-unmasker_en = pipeline("fill-mask", model="bert-base-uncased", device=device)
-res_en = unmasker_en(text, top_k=3)
+res_en = BERT('bert-base-uncased')
 show_topk(res_en, k=3)
+print("Метрика:", "OK" if test(res_en) else "MISS", '\n')
+
 
 print("Ру BERT")
-unmasker_ru = pipeline("fill-mask", model="DeepPavlov/rubert-base-cased", device=device)
-res_ru = unmasker_ru(text, top_k=3)
+res_ru = BERT('DeepPavlov/rubert-base-cased')
 show_topk(res_ru, k=3)
+print("Метрика:", "OK" if test(res_ru) else "MISS", '\n')
+
 
 print("Многоязычная BERT")
-unmasker_mbert = pipeline("fill-mask", model="bert-base-multilingual-cased", device=device)
-res_mbert = unmasker_mbert(text, top_k=3)
+res_mbert = BERT('bert-base-multilingual-cased')
 show_topk(res_mbert, k=3)
+print("Метрика:", "OK" if test(res_mbert) else "MISS", '\n')
 
-gold_variants = {"России", "Российской Федерации"}
-pred_tokens = {r["token_str"].strip() for r in res_ru[:3]}
-hit = len(gold_variants & pred_tokens) > 0.
-print("\nМетрика (правильный вариант в топ-3 у русской модели):", "OK" if hit else "MISS")
+
+
